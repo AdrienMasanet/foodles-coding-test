2ef77756825c2ba691b3cff8445dc7ae12d50197
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-16 01:19:56 +0100

    feat: use a dictionnary to avoid nested loops

--- src/utils/countOccurences.ts
+++ src/utils/countOccurences.ts
@@ -10,35 +10,33 @@ export default function countOccurences(sentence: string, n: number): Word[] {
   // If n is 0 or a negative number, early return an empty array
   if (n <= 0) return [];
 
+  // Create a dictionary to store the words and their counts
+  const wordsDictionary: { [key: string]: number } = {};
+
+  // Split the sentence into an array of words and iterate over each word in the array to build the dictionary
+  sentence.split(" ").forEach((word) => {
+    // If word is an empty string, early return the current iteration
+    if (!word) return;
+
+    /* If the word is already in the dictionary, increment its count by 1
+     * otherwise add it to the dictionary with an initial count of 1 */
+    wordsDictionary[word] ? wordsDictionary[word]++ : (wordsDictionary[word] = 1);
+  });
+
   return (
-    sentence
+    Object.keys(wordsDictionary)
 
-      .split(" ") // Split the sentence into an array of words using spaces as the separator
+      // Build the final array of Word objects by collecting each entry from the dictionary
+      .map((word) => {
+        return { word, count: wordsDictionary[word] };
+      })
 
-      .reduce((accumulator: Word[], word: string): Word[] => {
-        // If word is an empty string, early return the current iteration
-        if (!word) return accumulator;
-
-        // If the word is already in the accumulator assign it to a new wordInAccumulator variable
-        const wordInAccumulator: Word | undefined = accumulator.find((wordInAccumulator) => wordInAccumulator.word === word);
-
-        /* If wordInAccumulator is not undefined (which means it is already in the building array), increment the count of the
-         * word in the accumulator and early return the current iteration */
-        if (wordInAccumulator) {
-          wordInAccumulator.count++;
-          return accumulator;
-        }
-
-        /* If wordInAccumulator is undefined, the if statement above was not executed so return the accumulator with the new word
-         * with the initial count of 1 */
-        return [...accumulator, { word, count: 1 }];
-      }, []) // The starting value of the accumulator is an empty array
-
+      // Sort the array by count descending and then by alphabetical order if count is equal
       .sort((firstWord: Word, secondWord: Word) => {
         if (firstWord.count !== secondWord.count) {
-          return secondWord.count - firstWord.count; // sort by count descending
+          return secondWord.count - firstWord.count;
         }
-        return firstWord.word.localeCompare(secondWord.word); // sort by alphabetical order if count is equal
+        return firstWord.word.localeCompare(secondWord.word);
       })
 
       // Slice the array to the first n elements only if n is less than the length of the array
